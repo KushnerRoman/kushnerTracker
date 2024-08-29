05312887acf0c0407a24597d99c1b8eb1f0aab89
Roman Kushner <73998196+KushnerRoman@users.noreply.github.com>
2024-08-29 18:55:38 +0300

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,11 @@ queryCreateTable();
 queryCreateTableBill();
 setupMiddleware(app);
 
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/handlers',express.static(path.join(__dirname, 'handlers')));
